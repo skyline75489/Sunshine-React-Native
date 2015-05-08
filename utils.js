6a84899a9a15833de8f6714905f6edd9d192b6e2
skyline75489 <skyline75489@outlook.com>
2015-05-08 17:39:01 +0800

    fake data OK

--- utils.js
+++ utils.js
@@ -39,3 +39,31 @@ exports.getIconForWeather = function(weatherId) {
   }
     return -1;
 }
+
+exports.getArtForTodayWeather = function(weatherId) {
+  if (weatherId >= 200 && weatherId <= 232) {
+    return 'art_storm';
+  } else if (weatherId >= 300 && weatherId <= 321) {
+    return 'art_light_rain';
+  } else if (weatherId >= 500 && weatherId <= 504) {
+    return 'art_rain';
+  } else if (weatherId == 511) {
+    return 'art_snow';
+  } else if (weatherId >= 520 && weatherId <= 531) {
+    return 'art_rain';
+  } else if (weatherId >= 600 && weatherId <= 622) {
+    return 'art_snow';
+  } else if (weatherId >= 701 && weatherId <= 761) {
+    return 'art_fog';
+  } else if (weatherId == 761 || weatherId == 781) {
+    return 'art_storm';
+  } else if (weatherId == 800) {
+    return 'art_clear';
+  } else if (weatherId == 801) {
+    return 'art_light_clouds';
+  } else if (weatherId >= 802 && weatherId <= 804) {
+    return 'art_cloudy';
+  }
+  return -1;
+}
+
